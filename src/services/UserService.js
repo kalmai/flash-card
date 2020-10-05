213fedeeb319858585ad51b00bd8c70efd338593
kalmai <leozverres@gmail.com>
2020-10-05 00:46:35 -0400

    updating base url for since i forgot

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const url = "https://flash-card-zrk.herokuapp.com";
-const url = "http://localhost:8080/";
+const url = "https://flash-card-zrk.herokuapp.com";
+// const url = "http://localhost:8080/";
 
 export default {
     login(userName){
